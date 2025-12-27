bf6e2030776ffd52d1b59062b0b8d3e7d0886734
HELAFUG <loxand42@gmail.com>
2025-12-27 14:19:21 +0300

    add auth backend

--- auth-service/backend/app/api/dependencies/__init__.py
+++ auth-service/backend/app/api/dependencies/__init__.py
@@ -1,7 +1,14 @@
-__all__ = ["get_access_tokens", "get_users", "get_strategy", "get_user_manager"]
+__all__ = [
+    "get_access_tokens",
+    "get_users",
+    "get_strategy",
+    "get_user_manager",
+    "authentication_backend",
+]
 
 from auth.strategy import get_strategy
 
 from .auth.access_tokens import get_access_tokens
+from .auth.backend import authentication_backend
 from .auth.user_manager import get_user_manager
 from .auth.users import get_users
---
+++ auth-service/backend/app/api/dependencies/auth/backend.py
@@ -0,0 +1,10 @@
+from core.authentication.transport import bearer_transport
+from fastapi_users.authentication import AuthenticationBackend
+
+from api.dependencies import get_strategy
+
+authentication_backend = AuthenticationBackend(
+    name="access-tokens-db",
+    transport=bearer_transport,
+    get_strategy=get_strategy,
+)
