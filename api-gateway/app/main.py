7c9e094f31dd66f185c2941c83bc087e88e208ec
HELAFUG <loxand42@gmail.com>
2025-12-28 01:11:08 +0300

    Update main.py

--- api-gateway/app/main.py
+++ api-gateway/app/main.py
@@ -1,9 +1,19 @@
 import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api import api_router
 from core.config import settings
-from fastapi import FastAPI
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
 
 
