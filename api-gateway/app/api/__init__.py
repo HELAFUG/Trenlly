a69eb3c2ab15c717783afce954fa7873a00daf78
HELAFUG <loxand42@gmail.com>
2025-12-27 17:55:17 +0300

    Update __init__.py

--- api-gateway/app/api/__init__.py
+++ api-gateway/app/api/__init__.py
@@ -1,4 +1,7 @@
 from core.config import settings
 from fastapi import APIRouter
 
+from .users import users_router
+
 api_router = APIRouter(prefix=settings.api_gateway.prefix)
+api_router.include_router(users_router)
