a5ed9c50663ac161415b96fdf63795ec41c3f75b
HELAFUG <loxand42@gmail.com>
2025-12-27 14:29:11 +0300

    Update __init__.py

--- auth-service/backend/app/api/__init__.py
+++ auth-service/backend/app/api/__init__.py
@@ -1,4 +1,8 @@
 from core.config import settings
 from fastapi import APIRouter
 
+from api.auth import router as auth_router
+
 api_router = APIRouter(prefix=settings.api.prefix)
+
+api_router.include_router(auth_router)
