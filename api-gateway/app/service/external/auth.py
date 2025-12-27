697d0ad9fcb3cbe0401b051967edc5b48ed2da7b
HELAFUG <loxand42@gmail.com>
2025-12-27 17:52:00 +0300

    add proxe method

---
+++ api-gateway/app/service/external/auth.py
@@ -0,0 +1,12 @@
+from aiohttp import ClientSession
+from core.config import settings
+from core.schemas.user import UserCreate
+
+
+async def proxy_auth_register(user: UserCreate):
+    async with ClientSession() as session:
+        async with session.post(
+            url=settings.external_services.user_service.register_url,
+            json=user.model_dump(exclude_unset=True),
+        ) as response:
+            return await response.json()
