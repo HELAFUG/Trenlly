41e1b329c68d8fcd9ca7dce1c3d0eb603e453bb8
HELAFUG <loxand42@gmail.com>
2025-12-27 12:24:37 +0300

    Create user_manager.py

---
+++ auth-service/backend/app/core/authentication/user_manager.py
@@ -0,0 +1,20 @@
+import logging
+
+from fastapi import Request
+from fastapi_users import BaseUserManager
+
+from core.config import settings
+from core.models import User
+from core.models.mixins import IDIntPKMixin
+
+log = logging.getLogger(__name__)
+
+
+class UserManager(IDIntPKMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = settings.access_token.reset_password_token_secret
+    verification_token_secret = settings.access_token.verification_token_secret
+
+    async def on_after_register(
+        self, user: User, request: Request | None = None
+    ) -> None:
+        log.info("User signed up %s", user.id)
