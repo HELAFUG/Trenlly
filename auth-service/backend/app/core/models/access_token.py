c2cd53e12fab3d92b082c1b3ecc6d6dfe43cb710
HELAFUG <loxand42@gmail.com>
2025-12-27 12:09:59 +0300

    Create access_token.py

---
+++ auth-service/backend/app/core/models/access_token.py
@@ -0,0 +1,28 @@
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyAccessTokenDatabase,
+    SQLAlchemyBaseAccessTokenTable,
+)
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.models import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+    from core.models import User
+
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTable):
+    user_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    user: Mapped["User"] = relationship(back_populates="access_tokens")
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyAccessTokenDatabase(session, cls)
