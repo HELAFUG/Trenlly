14f3ca538fb94c941be2af28a698eca9d722c2a1
HELAFUG <loxand42@gmail.com>
2025-12-28 01:02:54 +0300

    add auth router

--- api-gateway/app/service/external/auth.py
+++ api-gateway/app/service/external/auth.py
@@ -1,4 +1,4 @@
-from aiohttp import ClientSession
+from aiohttp import ClientSession, FormData
 from core.config import settings
 from core.schemas.user import UserCreate
 
@@ -13,9 +13,21 @@ async def proxy_auth_register(user: UserCreate):
 
 
 async def proxy_auth_login(user: UserCreate):
+    form_data = FormData()
+    form_data.add_field("grant_type", "password")
+    form_data.add_field("username", user.email)
+    form_data.add_field("password", user.password)
+    form_data.add_field("scope", "")
+    form_data.add_field("client_id", "")
+    form_data.add_field("client_secret", "")
+
     async with ClientSession() as session:
         async with session.post(
             url=settings.external_services.user_service.login_url,
-            json=user.model_dump(exclude_unset=True),
+            data=form_data,
+            headers={
+                "Accept": "application/json",
+            },
         ) as response:
+            response.raise_for_status()
             return await response.json()
