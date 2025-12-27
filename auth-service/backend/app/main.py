8119e40c9b76b4fc3269eb0bea11692df9a73f67
HELAFUG <loxand42@gmail.com>
2025-12-27 14:31:52 +0300

    Create main.py

---
+++ auth-service/backend/app/main.py
@@ -0,0 +1,25 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from api import api_router
+from core.config import settings
+from core.models import db_helper
+from fastapi import FastAPI
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_helper.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.srv.host,
+        port=settings.srv.port,
+        reload=True,
+    )
