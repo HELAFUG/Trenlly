b155f35de1d670e7d1b531a740e1eea08c37baa5
HELAFUG <loxand42@gmail.com>
2025-12-27 12:11:40 +0300

    Update user.py

--- auth-service/backend/app/core/models/user.py
+++ auth-service/backend/app/core/models/user.py
@@ -1,9 +1,11 @@
+from mmap import ACCESS_COPY
 from typing import TYPE_CHECKING
 
 from fastapi_users_db_sqlalchemy import (
     SQLAlchemyBaseUserTable as SQLAlchemyBaseUserTableGeneric,
 )
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy.orm import Mapped, relationship
 
 from core.models import Base
 from core.models.mixins import IDIntPKMixin
@@ -11,12 +13,16 @@ from core.models.mixins import IDIntPKMixin
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
+    from core.models import AccessToken
+
 
 class SQLAlchemyBaseUserTable(SQLAlchemyBaseUserTableGeneric[int]):
     pass
 
 
 class User(Base, IDIntPKMixin, SQLAlchemyBaseUserTable):  # pyright: ignore[reportIncompatibleVariableOverride]
+    access_tokens: Mapped[list["AccessToken"]] = relationship(back_populates="user")
+
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, cls)
