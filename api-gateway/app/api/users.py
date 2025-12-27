6be9b1c87dffc0750a63cab72ac8c169196600a4
HELAFUG <loxand42@gmail.com>
2025-12-27 18:01:06 +0300

    Update users.py

--- api-gateway/app/api/users.py
+++ api-gateway/app/api/users.py
@@ -3,11 +3,16 @@ from typing import Annotated
 from core.config import settings
 from core.schemas.user import UserCreate
 from fastapi import APIRouter, Depends
-from service.external.auth import proxy_auth_register
+from service.external.auth import proxy_auth_login, proxy_auth_register
 
 users_router = APIRouter(prefix=settings.api_gateway.auth)
 
 
 @users_router.post("/register")
-async def register_user(user: Annotated[UserCreate, Depends]):
+async def register_user(user: Annotated[UserCreate, Depends()]):
     return await proxy_auth_register(user)
+
+
+@users_router.post("/login")
+async def login_user(user: Annotated[UserCreate, Depends()]):
+    return await proxy_auth_login(user)
