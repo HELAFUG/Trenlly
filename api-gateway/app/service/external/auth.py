85ec1afbe0280d85d5b1611929de731379f58f50
HELAFUG <loxand42@gmail.com>
2025-12-27 17:58:57 +0300

    Update auth.py

--- api-gateway/app/service/external/auth.py
+++ api-gateway/app/service/external/auth.py
@@ -10,3 +10,12 @@ async def proxy_auth_register(user: UserCreate):
             json=user.model_dump(exclude_unset=True),
         ) as response:
             return await response.json()
+
+
+async def proxy_auth_login(user: UserCreate):
+    async with ClientSession() as session:
+        async with session.post(
+            url=settings.external_services.user_service.login_url,
+            json=user.model_dump(exclude_unset=True),
+        ) as response:
+            return await response.json()
