e3eeabffafa449d7e911efc1a134b2e0d14d5339
HELAFUG <loxand42@gmail.com>
2026-01-02 13:44:09 +0300

    it is useless

--- auth-service/backend/app/service/kafka/users.py
+++ auth-service/backend/app/service/kafka/users.py
@@ -7,7 +7,6 @@ log = logging.getLogger(__name__)
 
 
 async def after_user_registered(email: str):
-    log.info("User signed up %s", email)
     await fs_broker.publish(
         topic=settings.broker.after_register_topic,
         message={"email": email},
