a221d521616466298eb5a0d59b92cbe9abf9e34c
HELAFUG <loxand42@gmail.com>
2026-01-02 13:54:44 +0300

    add router to broker

--- notify-service/app/core/fs_broker.py
+++ notify-service/app/core/fs_broker.py
@@ -1,7 +1,9 @@
 from faststream.kafka import KafkaBroker
+from service.auth import auth_router
 
 from core.config import settings
 
 fs_broker = KafkaBroker(
     bootstrap_servers=settings.broker.bootstrap_servers,
 )
+fs_broker.include_router(auth_router)
