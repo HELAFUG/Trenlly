071bb362fcdec62982351670c3df041b057732cb
HELAFUG <loxand42@gmail.com>
2026-01-02 13:50:58 +0300

    change the topic name

--- notify-service/app/core/config.py
+++ notify-service/app/core/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class BrokerConfig(BaseModel):
     bootstrap_servers: str = "localhost:9092"
-    after_register_topic: str = "after_register"
+    after_register_topic: str = "after_registration"
 
 
 class Settings(BaseSettings):
