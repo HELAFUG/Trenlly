991f9aa799388881b30751b7c8d56d04e0ffead3
HELAFUG <loxand42@gmail.com>
2026-01-02 14:18:10 +0300

    add after login

--- notify-service/app/service/auth/register.py
+++ notify-service/app/service/auth/register.py
@@ -1,6 +1,6 @@
 from core.config import settings
 from faststream.kafka import KafkaRouter
-from mailing import send_welcome_email
+from mailing import send_login_email, send_welcome_email
 
 auth_router = KafkaRouter()
 
@@ -11,3 +11,7 @@ async def after_register(message: dict):
     await send_welcome_email(email)
 
 
+@auth_router.subscriber(settings.broker.after_login_topic)
+async def after_login(message: dict):
+    email = message.get("email")
+    await send_login_email(email)
