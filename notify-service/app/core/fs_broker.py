e5187da8f2b724f6a76eaed4b92f366efd0f3af3
HELAFUG <loxand42@gmail.com>
2026-01-03 13:28:26 +0300

    Update fs_broker.py

--- notify-service/app/core/fs_broker.py
+++ notify-service/app/core/fs_broker.py
@@ -1,9 +1,7 @@
 from faststream.kafka import KafkaBroker
-from service.auth import auth_router
 
 from core.config import settings
+from service.auth import auth_router
 
-fs_broker = KafkaBroker(
-    bootstrap_servers=settings.broker.bootstrap_servers,
-)
+fs_broker = KafkaBroker(bootstrap_servers=settings.broker.bootstrap_servers, acks="all")
 fs_broker.include_router(auth_router)
