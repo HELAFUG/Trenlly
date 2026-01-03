788a197421fb9f8f0ab0cdce8f9463aabf2233e6
HELAFUG <loxand42@gmail.com>
2026-01-03 13:32:19 +0300

    add auto offset reset

--- notify-service/app/service/auth/register.py
+++ notify-service/app/service/auth/register.py
@@ -5,13 +5,15 @@ from mailing import send_login_email, send_welcome_email
 auth_router = KafkaRouter()
 
 
-@auth_router.subscriber(settings.broker.after_register_topic)
+@auth_router.subscriber(
+    settings.broker.after_register_topic, auto_offset_reset="earliest"
+)
 async def after_register(message: dict):
     email = message.get("email")
     await send_welcome_email(email)
 
 
-@auth_router.subscriber(settings.broker.after_login_topic)
+@auth_router.subscriber(settings.broker.after_login_topic, auto_offset_reset="earliest")
 async def after_login(message: dict):
     email = message.get("email")
     await send_login_email(email)
