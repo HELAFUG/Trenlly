9c6027d27be0eb1d6bdb49dd6c3fde314a90fa14
HELAFUG <loxand42@gmail.com>
2026-01-04 10:49:42 +0300

    add naming conventions

--- person-service/app/core/config.py
+++ person-service/app/core/config.py
@@ -21,6 +21,13 @@ class SRVConfig(BaseModel):
 class DBConfig(BaseModel):
     url: str = getenv("DATABASE_URL", "")
     echo: bool = False
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class LoggingConfig(BaseModel):
--- person-service/app/core/models/base.py
+++ person-service/app/core/models/base.py
@@ -1,10 +1,15 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 from utils.case_converter import camel_case_to_snake_case
+
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
