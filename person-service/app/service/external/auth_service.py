35c4736976d99d97720f778ad6e0956220aaa822
HELAFUG <loxand42@gmail.com>
2026-01-04 11:57:23 +0300

    add proxy func to check user

---
+++ person-service/app/service/external/auth_service.py
@@ -0,0 +1,24 @@
+from aiohttp import ClientSession, FormData
+from core.config import settings
+from core.schemas.person import PersonCreate
+
+
+async def proxy_auth_login(person: PersonCreate):
+    form_data = FormData()
+    form_data.add_field("grant_type", "password")
+    form_data.add_field("username", person.email)
+    form_data.add_field("password", person.password)
+    form_data.add_field("scope", "")
+    form_data.add_field("client_id", "")
+    form_data.add_field("client_secret", "")
+
+    async with ClientSession() as session:
+        async with session.post(
+            url=settings.external_service.auth_service.login_url,
+            data=form_data,
+            headers={
+                "Accept": "application/json",
+            },
+        ) as response:
+            response.raise_for_status()
+            return await response.json()
