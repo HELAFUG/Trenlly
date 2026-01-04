b460836a6af6e8e845ee67d45f8f29036428a802
HELAFUG <loxand42@gmail.com>
2026-01-04 11:13:02 +0300

    create schemas for person

---
+++ person-service/app/core/schemas/person.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class PersonBase(BaseModel):
+    email: str
+    name: str
+    age: int
+
+
+class PersonCreate(PersonBase):
+    password: str
+    last_name: Optional[str]
+
+
+class Person(PersonBase):
+    id: int
+    last_name: Optional[str]
