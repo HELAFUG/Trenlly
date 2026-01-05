e9b96f64f0d3c468be25a23ad6efad30e8603b88
HELAFUG <loxand42@gmail.com>
2026-01-05 16:15:19 +0300

    Update goal.py

--- person-service/app/core/schemas/goal.py
+++ person-service/app/core/schemas/goal.py
@@ -6,11 +6,16 @@ from pydantic import BaseModel
 class GoalBase(BaseModel):
     name: str
     description: str
-    deadline_day: str
+    deadline_day: datetime.datetime
 
 
 class GoalCreate(GoalBase):
     pass
+
+
+class Goal(GoalBase):
+    id: int
+    created_at: datetime.datetime
 
 
 class GoalUpdate(GoalBase):
