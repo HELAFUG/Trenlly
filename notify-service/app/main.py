c70c63882882570944194b847139c2e99acbef1c
HELAFUG <loxand42@gmail.com>
2026-01-06 17:13:15 +0300

    add main func implementation

--- notify-service/app/main.py
+++ notify-service/app/main.py
@@ -1,4 +1,14 @@
+import asyncio
+
 from core import fs_broker
 from faststream import FastStream
 
 app = FastStream(fs_broker)
+
+
+async def main():
+    await app.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
